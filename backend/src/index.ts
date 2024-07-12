1448ab01ad73b3a18e84b87f9fb4ef111e55f9af
jainam-b <jainambagrecha16@gmail.com>
2024-07-12 11:43:05 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,24 +15,6 @@ app.post(" /api/v1/user/signup", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post(" /api/v1/user/signup", (c) => {
-  return c.text("Hello Hono!");
-});
 
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
-});
 
 export default app;
